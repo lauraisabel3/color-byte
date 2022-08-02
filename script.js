3987cb20f967f29172e2a01ad9e730cf3a293a3a
lauraisabel3 <lauraisabel543@gmail.com>
2022-08-02 17:51:08 -0500

    Second version

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
-// let img = document.getElementById('main__img');
-
-
-
-let input = document.getElementById('comment__input').value;
-
-console.log('input',input);
-
-
 async function fetchImgs() {
   let containerImgs =  document.querySelector("#slider");
   const response = await fetch('https://picsum.photos/v2/list?page=2&limit=100');
   const json = await response.json();
   const imgs = await json.map(el => {
-    // img.src = el.download_url;
     let img = document.createElement("img");
     img.src = el.download_url;
-    img.width = el.width;
-    img.height = el.height;
-    
-    console.log(img.width)
-    containerImgs.appendChild(img)
+    img.classList.add('swiper-slide');
+   
+    containerImgs.appendChild(img);
   }
   )
 }
 
+fetchImgs()
+
+let icon = document.getElementById('icon__like');
+const changeColorIcon = () => {
+  if (icon.classList.contains('icon__like-white')) {
+    icon.classList.replace('icon__like-white', 'icon__like-red')
+  } else {
+    icon.classList.replace('icon__like-red', 'icon__like-white')
+  }
+}
+icon.addEventListener('click', changeColorIcon)
+
+
 
 let container = document.getElementById('comments__container');
-
-let comment = document.getElementById('comment');
 
 const getComment = () => {
   let inputValue = document.getElementById('comment__input').value;
   let newComment = document.createElement("p")
   let text = document.createTextNode(inputValue);
   
-  newComment.appendChild(text)
-  console.log('type', typeof newComment, newComment)
-  container.appendChild(newComment)
+  newComment.appendChild(text);
+  newComment.textContent != "" && container.appendChild(newComment);
 
+  inputValue = '';
 }
 
+
+
+let button = document.getElementById('submit')
+button.addEventListener('click', submit)
+
 let commentButton = document.getElementById("comment__button");
-commentButton.addEventListener("click", getComment)
-fetchImgs()
+commentButton.addEventListener("click", getComment, submit )
+
 
